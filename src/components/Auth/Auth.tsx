bf1e00f319cc4442f1b2c43b8a8f42790ad0fa5b
fleshaaa <mainflesha@gmail.com>
2021-08-30 15:20:03 +0300

    initizlized

--- src/components/Auth/Auth.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const FormDialog: FC = () => {
-  return <>{"to do"}</>;
-};
-
-export default FormDialog;
--- src/components/hooks/useTypeSelector.ts
+++
@@ -1,3 +0,0 @@
-import { RootState } from '../../store/reducers/index'
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-export const useTypeSelector: TypedUseSelectorHook<RootState> = useSelector
